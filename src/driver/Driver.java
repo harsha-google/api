83bd5866dac03772fb498f39129d1cea40db51b1
harsha <harshagithub@gmail.com>
2020-05-02 20:30:08 +0530

    first commit

---
+++ src/driver/Driver.java
@@ -0,0 +1,72 @@
+package driver;
+
+public class Driver {
+
+
+
+    public static void main(String[] args) throws Exception {
+        try {
+
+            Login();
+
+
+        }
+
+        catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+
+    }
+
+
+
+
+
+    public static void Login() throws Exception {
+        try {
+
+
+            System.out.println("new demo");
+            System.out.println("new demo");
+            System.out.println("new demo");
+            System.out.println("new demo");
+            System.out.println("new demo");
+            System.out.println("new demo");
+            System.out.println("new demo");
+            System.out.println("new demo");
+
+
+
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
